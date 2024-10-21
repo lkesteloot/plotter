59b7c89fc7af3a5cd200cb68d1e0a73c2b622dcb
Lawrence Kesteloot <lk@teamten.com>
2024-10-20 17:51:36 -0700

    Fix example for Python 3.

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 
 import math
 
-print "Sine*Exp\tCosine"
+print("Sine*Exp\tCosine")
 
 t = 0
 while t < 20:
-    print math.sin(t)*math.exp(-t*0.1), math.cos(t)
+    print(math.sin(t)*math.exp(-t*0.1), math.cos(t))
     t += 0.1
 
